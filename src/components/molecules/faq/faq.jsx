ec6d0178072cc819e6cb248752b6c7494d07371e
RickDeb2004 <debanjanrick04@gmail.com>
2023-08-25 11:47:22 +0530

    updated faq.jsx , FaqItem.jsx

--- src/components/molecules/faq/faq.jsx
+++ src/components/molecules/faq/faq.jsx
@@ -5,77 +5,77 @@ import FaqItem from '../../atoms/faq/FaqItem';
 import FaqItem from '../../atoms/faq/FaqItem';
 import { FaqContainer,FaqHeading,Caption,FaqLayout, FaqItem, Answer } from './style';
 
-const FaqContainer = styled.div`
-  padding: 25px;
+// const FaqContainer = styled.div`
+//   padding: 25px;
 
-  @media (max-width: 768px) {
-    padding: 15px;
-  }
-`;
+//   @media (max-width: 768px) {
+//     padding: 15px;
+//   }
+// `;
 
-const FaqHeading = styled.div`
-font-weight: 700;
-font-size: 54px;
-font-family:Roboto;
-font-style:normal;
-text-align: left; 
-padding-right: 25px; 
-margin-bottom:160px;
+// const FaqHeading = styled.div`
+// font-weight: 700;
+// font-size: 54px;
+// font-family:Roboto;
+// font-style:normal;
+// text-align: left; 
+// padding-right: 25px; 
+// margin-bottom:160px;
 
-@media (max-width: 768px) {
-  font-size: 36px;
-  padding-right: 15px; 
-  margin-bottom:80px;
-}
-`;
-
-
+// @media (max-width: 768px) {
+//   font-size: 36px;
+//   padding-right: 15px; 
+//   margin-bottom:80px;
+// }
+// `;
 
 
 
-const Caption = styled.div`
- margin-bottom:116px;
-    font-size: 24px;
-    text-align:left;
-    font-syle:normal;
-    font-family: Roboto;
 
-`;
 
-const FaqLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
+// const Caption = styled.div`
+//  margin-bottom:116px;
+//     font-size: 24px;
+//     text-align:left;
+//     font-syle:normal;
+//     font-family: Roboto;
 
-  @media (max-width: 768px) {
-    gap: 16px;
-  }
-`;
+// `;
 
-const FaqItem = styled.div`
-  background-color: #f0f0f0;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 2px;
-  padding: 16px;
-  font-size: 22px;
-  cursor: pointer;
-  transition: background-color 0.3s;
+// const FaqLayout = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 24px;
 
-  &.active {
-    background-color: #d9d9d9;
-  }
-  > div:first-child { 
-    text-align: left; 
-  }
-`;
+//   @media (max-width: 768px) {
+//     gap: 16px;
+//   }
+// `;
 
-const Answer = styled.div`
-  font-size: 22px;
-  padding-top: 16px;
-  display: ${(props) => (props.active ? 'block' : 'none')};
-`;
+// const FaqItem = styled.div`
+//   background-color: #f0f0f0;
+//   width: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 2px;
+//   padding: 16px;
+//   font-size: 22px;
+//   cursor: pointer;
+//   transition: background-color 0.3s;
+
+//   &.active {
+//     background-color: #d9d9d9;
+//   }
+//   > div:first-child { 
+//     text-align: left; 
+//   }
+// `;
+
+// const Answer = styled.div`
+//   font-size: 22px;
+//   padding-top: 16px;
+//   display: ${(props) => (props.active ? 'block' : 'none')};
+// `;
 
 const FaqComponent = () => {
   const faqs = [

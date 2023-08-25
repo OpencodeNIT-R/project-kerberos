e34594002ef1b6c78acee776582fc47529696ec8
Arya <bhattacharyyaarya165@gmail.com>
2023-08-25 20:59:21 +0530

    Changed

--- src/pages/Playground/index.jsx
+++ src/pages/Playground/index.jsx
@@ -2,7 +2,7 @@ import MentorCarousel from '../../components/molecules/MentorCarousel/MentorCaro
 import { Body2, Body3, Button, ButtonMeta, Heading1, Heading2, Heading3, Heading4, NavText } from '../../components/shared';
 import Body1 from '../../components/shared/Typography/Body1';
 import { Container } from './styles';
-import Hero from '../components/molecules/Hero/Hero';
+
 
 
 const Playground = () => {
@@ -22,7 +22,7 @@ const Playground = () => {
         <Button text='Primary Button' variant='secondary' />
       </Container>
       <MentorCarousel />
-      <Hero/>
+      
     </>
   );
 };

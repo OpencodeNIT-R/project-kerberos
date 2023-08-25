a48aaf194e225af10b633e96b71fd549a19503e7
RickDeb2004 <debanjanrick04@gmail.com>
2023-08-25 22:33:37 +0530

    updated faq.jsx, FaqItem.jsx

--- src/components/atoms/faq/FaqItem.jsx
+++ src/components/atoms/faq/FaqItem.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { FaqItemContainer, Answer} from './style';
 
+//const FaqItem = ({ faq, isActive, onClick }) => {
+   // return (
+      //  <FaqItemContainer className={isActive ? 'active' : ''} onClick={onClick}>
 
+const FaqItem =({faq}) => {
+    const [isActive, setIsActive] = useState(false);
+    const toggleFaq = () => {
+        setIsActive(!isActive);
+      };
 
-const FaqItem = ({ faq, isActive, onClick }) => {
-  return (
-    <FaqItemContainer className={isActive ? 'active' : ''} onClick={onClick}>
+    return (
+    <FaqItemContainer className={isActive ? 'active' : ''} onClick={toggleFaq}>
       <div>{faq.question}</div>
       <Answer active={isActive}>{faq.answer}</Answer>
     </FaqItemContainer>
--- src/components/molecules/faq/faq.jsx
+++ src/components/molecules/faq/faq.jsx
@@ -12,21 +12,21 @@ const FaqComponent = () => {
   const faqs = Object.values(faqData);
   
 
-  const [activeIndex, setActiveIndex] = useState([]);
+  //const [activeIndex, setActiveIndex] = useState([]);
 
-  const toggleFaq = (index) => {
-    const newActiveIndexes=[...activeIndex];
+//   const toggleFaq = (index) => {
+//     const newActiveIndexes=[...activeIndex];
    
-   if(newActiveIndexes.includes(index)){
-    const indexToRemove=newActiveIndexes.indexOf(index);
-    newActiveIndexes.splice(indexToRemove,1);    
-   }
-    else{
-        newActiveIndexes.push(index);
-        }
-    setActiveIndex(newActiveIndexes);
+//    if(newActiveIndexes.includes(index)){
+//     const indexToRemove=newActiveIndexes.indexOf(index);
+//     newActiveIndexes.splice(indexToRemove,1);    
+//    }
+//     else{
+//         newActiveIndexes.push(index);
+//         }
+//     setActiveIndex(newActiveIndexes);
 
-};
+// };
 
   return (
     <FaqContainer>
@@ -37,14 +37,15 @@ const FaqComponent = () => {
           <FaqItem
           
             key={index}
-            faq={faq}
-            isActive={activeIndex.includes(index)}
+            faq={faq}/>
+           // isActive={activeIndex.includes(index)}
+        
 
-            onClick={() => toggleFaq(index)}
-          >
-            <div>{faq.question}</div>
-            <Answer active={activeIndex.includes(index)}>{faq.answer}</Answer>
-          </FaqItem>
+          //   onClick={() => toggleFaq(index)}
+          // >
+          //   <div>{faq.question}</div>
+          //   <Answer active={activeIndex.includes(index)}>{faq.answer}</Answer>
+          //</FaqItem>
         ))}
       </FaqLayout>
     </FaqContainer>

385b9dbd590e4054b3e3bd4715aff7e63ca4066c
RickDeb2004 <debanjanrick04@gmail.com>
2023-08-25 18:18:59 +0530

    comment ommited

--- src/components/atoms/faq/FaqItem.jsx
+++ src/components/atoms/faq/FaqItem.jsx
@@ -2,30 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FaqItemContainer, Answer} from './style';
 
-// const FaqItemContainer = styled.div`
-//   background-color: #f0f0f0;
-//   width: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   gap: 2px;
-//   padding: 16px;
-//   font-size: 22px;
-//   cursor: pointer;
-//   transition: background-color 0.3s;
 
-//   &.active {
-//     background-color: #d9d9d9;
-//   }
-//   > div:first-child { 
-//     text-align: left; 
-//   }
-// `;
-
-// const Answer = styled.div`
-//   font-size: 22px;
-//   padding-top: 16px;
-//   display: ${(props) => (props.active ? 'block' : 'none')};
-// `;
 
 const FaqItem = ({ faq, isActive, onClick }) => {
   return (

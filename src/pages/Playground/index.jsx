f792f8649cddc53f16e51030ba9fad063d707485
starkjay21 <starkjay2107@gmail.com>
2023-08-27 22:57:06 +0530

    fix: removed container wrap

--- src/pages/Playground/index.jsx
+++ src/pages/Playground/index.jsx
@@ -1,11 +1,6 @@
-import { Container } from './styles';
 import ResourceSection from '../../components/molecules/ResourceSection/ResourceSection.jsx';
 
 const Playground = () => {
-  return (
-    <Container>
-      <ResourceSection />
-    </Container>
-  );
+  return <ResourceSection />;
 };
 export default Playground;

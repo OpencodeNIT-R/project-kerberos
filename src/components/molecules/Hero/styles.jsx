96d8a81c571cae768070b2bdfbe768e8412d98c5
Arya <bhattacharyyaarya165@gmail.com>
2023-08-27 19:33:45 +0530

    Updated and changed  Hero Section

--- src/components/molecules/Hero/styles.jsx
+++ src/components/molecules/Hero/styles.jsx
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 import wallpaper from './wallpaper.png';
 
-export const HeroWrapper = styled.div `
+export const HeroWrapper = styled.div`
   background-image: url(${wallpaper});
   background-size: cover;
   background-position: center;
+  background-repeat: no-repeat; /* Prevent background image from repeating */
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    background-size: 100% auto; /* Adjust to fit the entire width without cutting off */
+  }
 `;
 
-export const HeroContent = styled.div `
+export const HeroContent = styled.div`
   text-align: center;
   color: #fff;
   padding: 2rem;
@@ -21,8 +26,15 @@ export const HeroContent = styled.div `
     margin-bottom: 1rem;
   }
 
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
   @media (max-width: 768px) {
     padding: 1rem;
+    background-size: 100% auto;
+    
 
     h1 {
       font-size: 2rem;

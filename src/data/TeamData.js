e128b5a4f87689f9af3092a9331d8160090c4c15
SingAyush <anonymoussingh5665@gmail.com>
2024-06-14 17:42:13 +0530

    prettier check

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -2,64 +2,78 @@ export const TeamData = [
   {
     name: 'Brajesh Mohanty',
     Position: 'President',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.49.19_6615dea1_z1zr3g.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.49.19_6615dea1_z1zr3g.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/brajesh-mohanty-a2157227a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/brajesh-mohanty-a2157227a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
   },
   {
     name: 'Asish Das',
     Position: 'Vice-President',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364972/WhatsApp_Image_2024-06-14_at_16.49.59_da12d360_xkkztk.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364972/WhatsApp_Image_2024-06-14_at_16.49.59_da12d360_xkkztk.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/asishdas09?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/asishdas09?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
   },
   {
     name: 'Siddhartha Sahu',
     Position: 'Design Head',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.14_374ae203_oukjxl.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.14_374ae203_oukjxl.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/siddhartha-sahu-0a3631244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/siddhartha-sahu-0a3631244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
-  },  
+  },
   {
     name: 'S Harikrishna Panicker',
     Position: 'Strategic Head',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.50.24_6b90e0e2_iqsfhu.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.50.24_6b90e0e2_iqsfhu.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/s-harikrishna-panicker-544354205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/s-harikrishna-panicker-544354205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
   },
   {
     name: 'Laksh Garg',
     Position: 'Secretary',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.50.53_d8a5e5e6_x2ghcq.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.50.53_d8a5e5e6_x2ghcq.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/laksh-garg-96871b257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/laksh-garg-96871b257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
   },
   {
     name: 'Urvi Das',
     Position: 'Outreach Head',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.23_5d9ecda0_ohi4bb.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.23_5d9ecda0_ohi4bb.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/urvi-das-059988293?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/urvi-das-059988293?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
   },
   {
     name: 'Harsh Pawar',
     Position: 'Content Head',
-    imagesrc: 'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.06_17369877_e63msh.jpg',
+    imagesrc:
+      'https://res.cloudinary.com/dqbcbqcbr/image/upload/v1718364971/WhatsApp_Image_2024-06-14_at_16.51.06_17369877_e63msh.jpg',
     achievements: '',
     Instagram: '',
-    LinkedIn: 'https://www.linkedin.com/in/harsh-pawar-b84741276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    LinkedIn:
+      'https://www.linkedin.com/in/harsh-pawar-b84741276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     Twitter: '',
-  }, 
+  },
 ];
